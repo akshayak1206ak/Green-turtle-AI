247a538fbdaf8d575fee069fcc835ae5e565e366
Akshaya <akshaya@example.com>
2026-05-31 20:18:25 +0530

    Fixed Render deployment issues

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import sqlite3
 from flask_bcrypt import Bcrypt
 from database import init_db
 import os
-import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 
 app = Flask(__name__)
